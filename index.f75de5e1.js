f094a34b47d18077507bd87d3b20a7179ae1bfe4
natalia646 <75090544+natalia646@users.noreply.github.com>
2024-12-12 21:41:31 +0200

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,62 @@
+'use strict';
+const getElement = ([message, className])=>{
+    const element = document.createElement('div');
+    element.setAttribute('data-qa', 'notification');
+    element.innerText = message;
+    element.className = className;
+    return document.body.appendChild(element);
+};
+let leftClick = false;
+let rightClick = false;
+const promise1 = new Promise((resolve, reject)=>{
+    document.addEventListener('click', ()=>{
+        leftClick = true;
+        if (leftClick) resolve([
+            'First promise was resolved',
+            'success'
+        ]);
+    });
+    if (!leftClick) setTimeout(()=>{
+        // eslint-disable-next-line prefer-promise-reject-errors
+        reject([
+            'First promise was rejected',
+            'error'
+        ]);
+    }, 3000);
+});
+const promise2 = new Promise((resolve)=>{
+    document.addEventListener('click', ()=>{
+        resolve([
+            'Second promise was resolved',
+            'success'
+        ]);
+    });
+    document.addEventListener('contextmenu', (e)=>{
+        e.preventDefault();
+        resolve([
+            'Second promise was resolved',
+            'success'
+        ]);
+    });
+});
+const promise3 = new Promise((resolve)=>{
+    document.addEventListener('click', ()=>{
+        leftClick = true;
+        if (rightClick && leftClick) resolve([
+            'Third promise was resolved',
+            'success'
+        ]);
+    });
+    document.addEventListener('contextmenu', ()=>{
+        rightClick = true;
+        if (rightClick && leftClick) resolve([
+            'Third promise was resolved',
+            'success'
+        ]);
+    });
+});
+promise1.then(getElement).catch(getElement);
+promise2.then(getElement);
+promise3.then(getElement);
+
+//# sourceMappingURL=index.f75de5e1.js.map
